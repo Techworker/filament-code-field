92cf1fe1d7cbed9d537dd8fd2cb1b75d6a08b9f1
Techworker <benjaminansbach@googlemail.com>
2023-04-21 01:32:53 +0200

    Callbackable

--- src/Concerns/Autocompletion.php
+++ src/Concerns/Autocompletion.php
@@ -2,10 +2,12 @@
 
 namespace Creagia\FilamentCodeField\Concerns;
 
+use Illuminate\Contracts\Support\Arrayable;
+
 trait Autocompletion
 {
     public bool $autocompletion = true;
-    public array $autocompletionList = [];
+    public array | Arrayable | string | \Closure $autocompletionList = [];
 
     public function disableAutocompletion(): static
     {
@@ -14,12 +16,30 @@ trait Autocompletion
         return $this;
     }
 
+    public function getAutocompletionList() {
+        $autocompletionList = $this->evaluate($this->autocompletionList) ?? [];
+
+        if (is_string($autocompletionList) && function_exists('enum_exists') && enum_exists($autocompletionList)) {
+            $autocompletionList = collect($autocompletionList::cases())->mapWithKeys(static fn ($case) => [($case?->value ?? $case->name) => $case->name]);
+        }
+
+        if ($autocompletionList instanceof Arrayable) {
+            $autocompletionList = $options->toArray();
+        }
+
+        return $autocompletionList;
+    }
+
     public function addAutocompletionListItem(array $item) : static {
+        if(!is_array($this->autocompletionList)) {
+            $this->autocompletionList = [];
+        }
+
         $this->autocompletionList[] = $item;
         return $this;
     }
 
-    public function setAutocompletionListItems(array $items) : static {
+    public function setAutocompletionListItems(array | Arrayable | string | \Closure $items) : static {
         $this->autocompletionList = $items;
         return $this;
     }
